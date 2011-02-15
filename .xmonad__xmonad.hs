35ed7d1d9add956d31f2913261bd2522af7f75fb
Brett Hoerner <brett@bretthoerner.com>
2011-02-15 11:08:31 -0600

    Enable ResizableTile while keeping Gnome friendliness.

--- .xmonad__xmonad.hs
+++ .xmonad__xmonad.hs
@@ -1,22 +1,21 @@
 -- To replace Metacity,
 -- gconftool-2 -s /desktop/gnome/session/required_components/windowmanager xmonad --type string
 
--- TODO: http://xmonad.org/xmonad-docs/xmonad-contrib/XMonad-Actions-UpdatePointer.html
-
 import XMonad
 import XMonad.Config.Gnome (gnomeConfig)
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Hooks.ManageHelpers (isDialog, isFullscreen, doCenterFloat, doFullFloat)
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.ResizableTile
 
 import qualified Data.Map as M
 
-myLayout =  ResizableTall 1 (3/100) (1/2) []
-myKeys conf@(XConfig {XMonad.modMask = modm}) =
-       [ ((modm, xK_a), sendMessage MirrorShrink)
-       , ((modm, xK_z), sendMessage MirrorExpand) ]
-newKeys x  = M.union (keys defaultConfig x) (M.fromList (myKeys x))
+myLayout =  avoidStruts (ResizableTall 1 (3/100) (1/2) [])
+myKeys conf@(XConfig {XMonad.modMask = mod4Mask}) =
+       [ ((mod4Mask, xK_a), sendMessage MirrorShrink)
+       , ((mod4Mask, xK_z), sendMessage MirrorExpand) ]
+newKeys x  = M.union (keys gnomeConfig x) (M.fromList (myKeys x))
 myManageHook :: [ManageHook]
 myManageHook =
     [ resource  =? "Do" --> doIgnore
@@ -26,8 +25,8 @@ myManageHook =
 
 main = xmonad gnomeConfig
     { manageHook = manageHook gnomeConfig <+> composeAll myManageHook
---  , layoutHook = myLayout
---  , keys = newKeys
+    , layoutHook = myLayout
+    , keys = newKeys
     , focusFollowsMouse = False
     , modMask = mod4Mask
     , startupHook = ewmhDesktopsStartup >> setWMName "LG3D"
