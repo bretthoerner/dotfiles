750ca5099a38c1d60761c55fac5a3acce915ac01
Brett Hoerner <brett@bretthoerner.com>
2011-02-15 12:52:42 -0600

    Add DynamicWorkspaces to Xmonad config and switch to EZConfig.

--- .xmonad__xmonad.hs
+++ .xmonad__xmonad.hs
@@ -2,20 +2,20 @@
 -- gconftool-2 -s /desktop/gnome/session/required_components/windowmanager xmonad --type string
 
 import XMonad
+import XMonad.Actions.DynamicWorkspaces
 import XMonad.Config.Gnome (gnomeConfig)
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Hooks.ManageHelpers (isDialog, isFullscreen, doCenterFloat, doFullFloat)
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.ResizableTile
+import XMonad.Prompt (defaultXPConfig)
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig (additionalKeysP)
 
 import qualified Data.Map as M
 
 myLayout =  avoidStruts (ResizableTall 1 (3/100) (1/2) [])
-myKeys conf@(XConfig {XMonad.modMask = mod4Mask}) =
-       [ ((mod4Mask, xK_a), sendMessage MirrorShrink)
-       , ((mod4Mask, xK_z), sendMessage MirrorExpand) ]
-newKeys x  = M.union (keys gnomeConfig x) (M.fromList (myKeys x))
 myManageHook :: [ManageHook]
 myManageHook =
     [ resource  =? "Do" --> doIgnore
@@ -23,11 +23,17 @@ myManageHook =
     , isFullscreen --> doFullFloat
     , isDialog --> doCenterFloat ]
 
-main = xmonad gnomeConfig
+main = xmonad $ gnomeConfig
     { manageHook = manageHook gnomeConfig <+> composeAll myManageHook
     , layoutHook = myLayout
-    , keys = newKeys
     , focusFollowsMouse = False
     , modMask = mod4Mask
     , startupHook = ewmhDesktopsStartup >> setWMName "LG3D"
     , borderWidth = 3 }
+    `additionalKeysP`
+    [ ("M-a", sendMessage MirrorShrink)
+    , ("M-z", sendMessage MirrorExpand)
+    , ("M-d", removeWorkspace)
+    , ("M-r", renameWorkspace defaultXPConfig)
+    , ("M-'", selectWorkspace defaultXPConfig)
+    , ("M-S-'", withWorkspace defaultXPConfig (windows . W.shift)) ]
