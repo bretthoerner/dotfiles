2dafeab987a93c5e2ac3e41d60180516237de07f
Brett Hoerner <brett@bretthoerner.com>
2011-04-29 16:51:46 -0500

    Fix ipython copy/paste under OS X.

--- .ipython__copy_and_paste.py
+++ .ipython__copy_and_paste.py
@@ -5,11 +5,11 @@
 
 def copy(data):
     from subprocess import Popen, PIPE
-    Popen(["xclip", "-selection", "clipboard"], stdin=PIPE).communicate(str(data))
+    Popen(["pbcopy"], stdin=PIPE).communicate(str(data))
 
 def paste():
     from subprocess import Popen, PIPE
-    return Popen(["xclip", "-selection", "clipboard", "-o"], stdout=PIPE).communicate()[0]
+    return Popen(["pbpaste"], stdout=PIPE).communicate()[0]
 
 def ep():
     return eval(paste())
