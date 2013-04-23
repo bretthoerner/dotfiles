42ff009ae043b81bf9eb83a75d5813429b4a5acb
Brett Hoerner <brett@bretthoerner.com>
2013-04-23 18:22:29 -0500

    Update XMonad, XMobar, GTK-3.0 settings.

--- .xmonad__xmonad.hs
+++ .xmonad__xmonad.hs
@@ -2,11 +2,12 @@ import XMonad
 import XMonad.Actions.CopyWindow (copyToAll, killAllOtherCopies, kill1)
 import XMonad.Actions.CycleWS (toggleWS)
 import XMonad.Actions.DynamicWorkspaces
-import XMonad.Config.Gnome (gnomeConfig)
+import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Hooks.ManageHelpers (isDialog, isFullscreen, doCenterFloat, doFullFloat)
 import XMonad.Hooks.SetWMName
+import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
 import XMonad.Prompt (defaultXPConfig)
 import qualified XMonad.StackSet as W
@@ -14,19 +15,23 @@ import XMonad.Util.EZConfig (additionalKeysP)
 
 import qualified Data.Map as M
 
-myLayout =  avoidStruts $ ResizableTall 1 (3/100) (2/3) [] ||| Full
+myLayout =  smartBorders $ avoidStruts $ ResizableTall 1 (3/100) (2/3) [] ||| Full
 myManageHook :: [ManageHook]
 myManageHook =
     [ resource  =? "gcalctool" --> doCenterFloat
     , isFullscreen --> doFullFloat
     , isDialog --> doCenterFloat ]
 
-main = xmonad $ gnomeConfig
-    { manageHook = manageHook gnomeConfig <+> composeAll myManageHook
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+myConfig = defaultConfig
+    { manageHook = manageHook defaultConfig <+> composeAll myManageHook
     , layoutHook = myLayout
+    , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
     , focusFollowsMouse = False
     , modMask = mod4Mask
     , startupHook = ewmhDesktopsStartup >> setWMName "LG3D"
+    , terminal = "gnome-terminal"
     , borderWidth = 1 }
     `additionalKeysP`
     [ ("M-a", sendMessage MirrorShrink)
@@ -38,5 +43,11 @@ main = xmonad $ gnomeConfig
     , ("M-0", toggleWS)
     , ("M-S-=", windows copyToAll)
     , ("M-=",  killAllOtherCopies)
-    , ("M--",  kill1) ]
+    , ("M--",  kill1)
+    , ("M-S-l", spawn "gnome-screensaver-command --lock")
+    , ("<XF86AudioLowerVolume>", spawn "amixer -c 0 set Master 3dB-")
+    , ("<XF86AudioRaiseVolume>", spawn "amixer -c 0 set Master 3dB+")
+    , ("<XF86AudioMute>", spawn "amixer -c 0 set Master toggle")]
+
+main = xmonad =<< statusBar "xmobar" xmobarPP toggleStrutsKey myConfig
 
