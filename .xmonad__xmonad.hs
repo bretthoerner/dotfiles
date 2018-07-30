a7c0f6af6af336fef3f6dd1f5eaea6d32ace2422
Brett Hoerner <brett@bretthoerner.com>
2018-07-30 12:14:41 -0500

    XMonad + Gnome.

--- .xmonad__xmonad.hs
+++ .xmonad__xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Actions.CopyWindow (copyToAll, killAllOtherCopies, kill1)
 import XMonad.Actions.CycleWS (toggleWS)
 import XMonad.Actions.DynamicWorkspaces
 import XMonad.Actions.GridSelect
+import XMonad.Config.Gnome (gnomeConfig)
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks (avoidStruts)
@@ -23,7 +24,7 @@ import qualified XMonad.StackSet as W
 (~=?) :: Eq a => Query [a] -> [a] -> Query Bool
 q ~=? x = fmap (isPrefixOf x) q
 
-myLayout =  smartBorders $ avoidStruts $ ResizableTall 1 (3/100) (2/3) [] ||| Full
+-- myLayout =  smartBorders $ avoidStruts $ ResizableTall 1 (3/100) (2/3) [] ||| Full
 myManageHook :: [ManageHook]
 myManageHook =
     [ resource  =? "gcalctool" --> doCenterFloat
@@ -39,7 +40,7 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 myConfig = withUrgencyHook FocusHook $ ewmh defaultConfig
     { manageHook = manageHook defaultConfig <+> composeAll myManageHook
-    , layoutHook = myLayout
+    , layoutHook = smartBorders $ avoidStruts (layoutHook gnomeConfig)
     , handleEventHook = handleEventHook defaultConfig <+> fullscreenEventHook
     , focusFollowsMouse = False
     , modMask = mod4Mask
