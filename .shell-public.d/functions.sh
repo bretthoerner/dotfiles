5a7dfa51eb52f134b1c9c284420ecbe0c9002ec4
Brett Hoerner <brett@bretthoerner.com>
2019-09-06 08:34:59 -0500

    i3

--- .shell-public.d/functions.sh
+++ .shell-public.d/functions.sh
@@ -10,25 +10,24 @@ function install-rust-tools() {
 }
 
 function install-go-tools() {
+    go get -u -v github.com/acroca/go-symbols
+    go get -u -v github.com/cweill/gotests/...
+    go get -u -v github.com/fatih/gomodifytags
     go get -u -v github.com/go-delve/delve/cmd/dlv
-    go get -u -v github.com/stamblerre/gocode
-    go get -u -v golang.org/x/tools/cmd/gopls
-    go get -u -v github.com/nsf/gocode
-    go get -u -v github.com/rogpeppe/godef
+    go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
+    go get -u -v github.com/golang/mock/gomock
+    go get -u -v github.com/josharian/impl
     go get -u -v github.com/pressly/goose/cmd/goose
+    go get -u -v github.com/ramya-rao-a/go-outline
+    go get -u -v github.com/rogpeppe/godef
+    go get -u -v github.com/stamblerre/gocode
+    go get -u -v github.com/tpng/gopkgs
+    go get -u -v golang.org/x/lint/golint
     go get -u -v golang.org/x/tools/cmd/goimports
+    go get -u -v golang.org/x/tools/cmd/gopls
     go get -u -v golang.org/x/tools/cmd/gorename
     go get -u -v golang.org/x/tools/cmd/guru
-    go get -u -v github.com/golang/lint/golint
-    go get -u -v github.com/tpng/gopkgs
-    go get -u -v github.com/ramya-rao-a/go-outline
-    go get -u -v github.com/acroca/go-symbols
-    go get -u -v github.com/fatih/gomodifytags
     go get -u -v sourcegraph.com/sqs/goreturns
-    go get -u -v github.com/cweill/gotests/...
-    go get -u -v github.com/josharian/impl
-    go get -u -v github.com/golangci/golangci-lint/cmd/golangci-lint
-    go get github.com/golang/mock/gomock
     go install github.com/golang/mock/mockgen
 }
 
