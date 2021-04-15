d00f41781f278c8b5751eb39b1599581e71480f8
Brett Hoerner <brett@bretthoerner.com>
2021-04-15 14:34:56 +0000

    New alias.

--- .shell-public.d/alias.sh
+++ .shell-public.d/alias.sh
@@ -7,6 +7,9 @@ alias rm="rm -i"
 alias mv="mv -i"
 alias cp="cp -i"
 
+# show processes by cgroup
+alias psc='ps xawf -eo pid,user,cgroup,args'
+
 if which startx &> /dev/null; then
     alias x="ssh-agent -a ${HOME}/.ssh/agent_sock startx"
 fi
