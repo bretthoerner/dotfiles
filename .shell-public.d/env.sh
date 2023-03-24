0d0d1983a0062eb2a999a42f16ed02a6869795ec
Brett Hoerner <brett@bretthoerner.com>
2023-03-24 09:07:53 -0600

    nvm

--- .shell-public.d/env.sh
+++ .shell-public.d/env.sh
@@ -101,8 +101,8 @@ add-to-path-if-dir "${HOME}/Library/Application Support/multipass/bin"
 
 function load-nvm() {
     export NVM_DIR="$HOME/.nvm"
-    [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"  # This loads nvm
-    [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }
 
 # setup keychain ssh-agent for backup
